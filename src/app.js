0e394543565168c10703516a1f155cc1cb598bc0
1v1ad <1v1ad@proton.me>
2025-08-05 23:57:38 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes');
+const routes = require('./index'); // твой основной router
+
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://sweet-twilight-63a9b6.netlify.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use('/api', routes);
+
+// Тестовый корневой endpoint (по желанию)
+app.get('/', (req, res) => {
+  res.send('Backend API is running!');
+});
+
+// Тестовый endpoint для проверки API
+app.get('/api/ping', (req, res) => {
+  res.send('pong');
+});
 
 module.exports = app;
