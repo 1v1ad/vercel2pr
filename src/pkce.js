cf3687298f8af8c505cf44ef5ddebc82791090aa
Алексей Захаров <1v1ad@proton.me>
2025-08-11 16:02:17 +0300

    Очистка репозитория и заливка работающей авторизации

---
+++ src/pkce.js
@@ -0,0 +1,9 @@
+import crypto from 'crypto';
+function base64url(input){
+  return Buffer.from(input).toString('base64').replace(/=/g,'').replace(/\+/g,'-').replace(/\//g,'_');
+}
+export function createCodeVerifier(){ return base64url(crypto.randomBytes(64)); }
+export function createCodeChallenge(verifier){
+  const hash = crypto.createHash('sha256').update(verifier).digest();
+  return base64url(hash);
+}
