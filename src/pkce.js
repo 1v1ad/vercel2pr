4489803253c5a1ae36be11852b83f1d36d50f7fe
1v1ad <1v1ad@proton.me>
2025-08-22 22:48:17 +0300

    Update pkce.js

--- src/pkce.js
+++ src/pkce.js
@@ -1,9 +1,15 @@
 import crypto from 'crypto';
-function base64url(input){
-  return Buffer.from(input).toString('base64').replace(/=/g,'').replace(/\+/g,'-').replace(/\//g,'_');
+
+function b64url(buf) {
+  return Buffer.from(buf).toString('base64')
+    .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/g, '');
 }
-export function createCodeVerifier(){ return base64url(crypto.randomBytes(64)); }
-export function createCodeChallenge(verifier){
+
+export function createCodeVerifier(length = 64) {
+  return b64url(crypto.randomBytes(length));
+}
+
+export function createCodeChallenge(verifier) {
   const hash = crypto.createHash('sha256').update(verifier).digest();
-  return base64url(hash);
+  return b64url(hash);
 }
