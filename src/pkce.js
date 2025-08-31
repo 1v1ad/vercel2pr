f6c51a3575d661e4ec72389b2b9f7dc4a3843666
1v1ad <1v1ad@proton.me>
2025-08-31 19:15:16 +0300

    Delete src directory

--- src/pkce.js
+++
@@ -1,4 +0,0 @@
-import crypto from 'crypto';
-function base64url(buf){return Buffer.from(buf).toString('base64').replace(/\+/g,'-').replace(/\//g,'_').replace(/=+$/,'');}
-export function createCodeVerifier(){return base64url(crypto.randomBytes(32));}
-export function createCodeChallenge(verifier){const hash=crypto.createHash('sha256').update(verifier).digest();return base64url(hash);}
